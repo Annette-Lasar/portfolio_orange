3c8385f0cdc73548c06c92cec832bf6664bfa139
Annette Lasar <146415092+Annette-Lasar@users.noreply.github.com>
2026-01-25 08:06:44 +0100

    Add content in PrivacyPolicyComponent

--- src/app/main-layout/legal/privacy-policy/privacy-policy.ts
+++ src/app/main-layout/legal/privacy-policy/privacy-policy.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+
+import { MergedContent } from '../../../shared/interfaces/merged-content.interface';
+
+import { PageContentService } from '../../../shared/services/page-content.service';
+import { BreakpointObserver } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'port-privacy-policy',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './privacy-policy.html',
-  styleUrl: './privacy-policy.scss'
+  styleUrl: './privacy-policy.scss',
 })
-export class PrivacyPolicy {
+export class PrivacyPolicy implements OnInit{
+  mergedContent$!: Observable<MergedContent | null>;
+  private pageContentService = inject(PageContentService);
+  // private breakpointObserver = inject(BreakpointObserver);
 
+  ngOnInit(): void {
+    this.pageContentService.loadVariableContent('de');
+    this.pageContentService.loadStaticContent();
+
+    this.mergedContent$ = this.pageContentService.mergedContent$;
+  }
 }
--- src/app/shared/interfaces/legal.interface.ts
+++ src/app/shared/interfaces/legal.interface.ts
@@ -2,17 +2,70 @@ export interface ImprintContent {
   heading: string;
   subHeading1: string;
   subHeading2: string;
-  content: string;
   imprintOriginText: string;
   imprintOriginLink: string;
 }
 
+export interface PrivacyResponsible {
+  title: string;
+  text: string;
+}
+
+export interface PrivacyHosting {
+  title: string;
+  text: string;
+  dataTypes: string[];
+  legalBasis: string;
+  purpose: string;
+}
+
+export interface PrivacyContact {
+  title: string;
+  text: string;
+  dataTypes: string[];
+  legalBasis: string;
+  storageDuration: string;
+}
+
+export interface PrivacyDownloads {
+  title: string;
+  text: string;
+}
+
+export interface PrivacyLinks {
+  title: string;
+  text: string;
+}
+
+export interface PrivacyRights {
+  title: string;
+  items: string[];
+}
+
+export interface PrivacySecurity {
+  title: string;
+  text: string;
+}
+
+export interface PrivacyChanges {
+  title: string;
+  text: string;
+}
+
 export interface PrivacyPolicyContent {
   heading: string;
-  content: string;
+  asOf: string;
+  responsible: PrivacyResponsible;
+  hosting: PrivacyHosting;
+  contactForm: PrivacyContact;
+  downloads: PrivacyDownloads;
+  externalLinks: PrivacyLinks;
+  rights: PrivacyRights;
+  security: PrivacySecurity;
+  changes: PrivacyChanges;
 }
 
 export interface LegalContent {
   imprint: ImprintContent;
-  privacyPolicy: PrivacyPolicyContent;
+  privacy: PrivacyPolicyContent;
 }
